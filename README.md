4f3eed42fe021379973908c520274cd9f9cbf51f
mati <m.arocena97@gmail.com>
2020-08-07 13:33:44 -0300

    Implemented basic I/O menus, created .gitignore, created readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# NASM-CALC
+
+The goal of the project is to do a basic 16 bit calculator capable of doing substraction, addition and show a basic I/O menu.
+
+## TODO
+[x] I/O menu handeling
+[ ] Create makefile
+[ ] Implement actual 16 bit substraction
+[ ] Implement actual 16 bit addition
+[ ] Handle unexpected input
+
+## Compilation
+'''
+nasm -felf64 -g -dwarf calc.asm -o calc.o
+ld calc.o -o calc
+'''
--- calc.asm
+++ calc.asm
@@ -3,14 +3,23 @@ SECTION .DATA
 	menuLen:	equ $-menuStr
 	addItem:	db '1-Add', 0xA, 0xD
 	addItemLen:	equ $-addItem
+	subItem:	db '2-Sub', 0xA, 0xD
+	subItemLen: 	equ $-subItem
 	exitItem:	db '0-Exit', 0xA, 0xD
 	exitItemLen:	equ $-exitItem
+	inputNumber:	db 'Input a number:', 0xA, 0xD
+	inputNumberLen:	equ $-inputNumber
+	pressKey:	db 'Press any key to continue...', 0xA, 0xD
+	pressKeyLen:	equ $-pressKey
+	result:		db 'The result is:', 0xA, 0xD
+	resultLen:	equ $-result
 	readLen	equ 8
 
 SECTION .TEXT
 	global 	_start
 
 read:
+; read() -> buf:string buffer
 	push	rax
 	push	rbx
 	push	rcx
@@ -28,6 +37,7 @@ read:
 	ret
 
 print:
+; print(eax <- string buffer, ebx <- string lenght) -> void
 	push 	rcx
 	push	rdx
 	mov	ecx, eax
@@ -39,20 +49,114 @@ print:
 	pop	rcx
 	ret
 
+add:	
+	mov eax, addItem
+	mov ebx, addItemLen
+	call print
+
+	mov eax, inputNumber
+	mov ebx, inputNumberLen
+	call print
+	call read
+	mov ecx, buf
+	
+	mov eax, inputNumber
+	mov ebx, inputNumberLen
+	call print
+	call read
+	mov eax, buf
+	
+	;TODO: Proper sum
+	add eax, ecx
+	mov eax, ecx
+	
+	mov eax, result
+	mov ebx, resultLen
+	call print
+
+	; TODO: Proper sum
+	mov eax, ecx
+	mov ebx, 16
+	call print
+	
+	mov eax, pressKey
+	mov ebx, pressKeyLen
+	call print
+	call read 
+		
+	jmp _start	
+
+
+sub:
+	mov eax, subItem
+	mov ebx, subItemLen
+	call print
+
+	mov eax, inputNumber
+	mov ebx, inputNumberLen
+	call print
+	call read
+	mov ecx, buf
+	
+	mov eax, inputNumber
+	mov ebx, inputNumberLen
+	call print
+	call read
+	mov eax, buf
+	
+	;TODO: Proper sub
+	sub eax, ecx
+	mov eax, ecx
+	
+	mov eax, result
+	mov ebx, resultLen
+	call print
+
+	; TODO: Proper sub
+	mov eax, ecx
+	mov ebx, 16
+	call print
+	
+	mov eax, pressKey
+	mov ebx, pressKeyLen
+	call print
+	call read 
+		
+	jmp _start	
+
+
+
 _start:
 	nop
-	mov 	eax, menuStr
-	mov	ebx, menuLen
-	call	print
+	
+	mov 	eax, menuStr 
+	mov	ebx, menuLen 
+	call	print ; print(menuStr, menuLen)
+	
 	mov	eax, addItem
 	mov	ebx, addItemLen
-	call 	print
+	call 	print ; print(addItem, addItemLen)
+	
+	mov	eax, subItem
+	mov	ebx, subItemLen
+	call	print ; print(subItem, subItemLen)
+	
 	mov	eax, exitItem
 	mov	ebx, exitItemLen
-	call 	print
-	call	read
+	call 	print ; print(exitItem, exitItemLen)
+	
+	call	read ; read()
 	mov	eax, [buf]
-	cmp 	eax, 0xa30 
+
+	and	al, 0xF ; mask to get last nibble
+
+	cmp	al, 0x1 ; is add?
+	je 	add
+
+	cmp	al, 0x2 ; is sub?
+	je	sub
+
+	cmp 	al, 0x0 
 	jne	_start
 
 exit:
@@ -62,3 +166,4 @@ exit:
 
 section	.bss
 	buf:	resb 8
+
